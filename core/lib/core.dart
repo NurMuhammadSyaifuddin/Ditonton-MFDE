de30991d734a01793c2383f00e9fb1252e45a3c2
NurMuhammadSyaifuddin <nurm9301@gmail.com>
2023-02-19 11:18:29 +0700

    update

---
+++ core/lib/core.dart
@@ -0,0 +1,24 @@
+library core;
+
+export 'styles/colors.dart';
+export 'styles/text_styles.dart';
+export 'utils/constants.dart';
+export 'utils/exception.dart';
+export 'utils/failure.dart';
+export 'utils/state_enum.dart';
+export 'presentation/provider/movie/movie_detail_notifier.dart';
+export 'presentation/provider/movie/movie_list_notifier.dart';
+export 'presentation/provider/movie/now_playing_movies_notifier.dart';
+export 'presentation/provider/movie/popular_movies_notifier.dart';
+export 'presentation/provider/movie/top_rated_movies_notifier.dart';
+export 'presentation/provider/movie/watchlist_movie_notifier.dart';
+
+export 'presentation/widgets/movie_card_list.dart';
+export 'presentation/widgets/tvshow_card_list.dart';
+
+export 'presentation/provider/tvshow/airing_today_tvshow_notifier.dart';
+export 'presentation/provider/tvshow/popular_tvshow_notifier.dart';
+export 'presentation/provider/tvshow/top_rated_tvshow_notifier.dart';
+export 'presentation/provider/tvshow/tvshow_detail_notifier.dart';
+export 'presentation/provider/tvshow/tvshow_list_notifier.dart';
+export 'presentation/provider/tvshow/watchlist_tvshow_notifier.dart';
