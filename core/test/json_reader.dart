f79d6c36d62248f68a03519ced98d367ffbdb9f8
NurMuhammadSyaifuddin <nurm9301@gmail.com>
2023-02-20 23:08:51 +0700

    update

--- core/test/json_reader.dart
+++ core/test/json_reader.dart
@@ -1,14 +1,9 @@
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
-
 String readJson(String name) {
   var dir = Directory.current.path;
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
   }
-  if (!kDebugMode) {
-    dir += '/core';
-  }
-  return File('$dir/test/$name').readAsStringSync();
+  return File('$dir/core/test/$name').readAsStringSync();
 }
